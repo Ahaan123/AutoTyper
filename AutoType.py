56a8e9dcb329f357d0b6367d1e768e0e3bac0859
Ahaan123 <52870145+Ahaan123@users.noreply.github.com>
2020-07-28 21:47:37 +0200

    Update AutoType.py

--- AutoType.py
+++ AutoType.py
@@ -1,10 +1,9 @@
-import os
-import sys
 from pynput.keyboard import Key, Controller
 from time import sleep
-sleep(15)
-typing_string = "This is the typing string"
-send_amount = 5
+delay_before_type = 15 # This is the delay before auto-typing in seconds
+sleep(delay_before_type)
+typing_string = "This is the typing string" # This is the string which will be repeatedly typed
+send_amount = 5 # This is the number of times it will be typed
 keyboard = Controller()
 for i in range(send_amount):
     sleep(0.1)
